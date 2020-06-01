d44811f99b3bd2f65ce2792e8db7c79fff4e1aa8
kj_cho <kj_cho@idealidea.co.kr>
2020-06-01 12:09:26 +0900

    서버에서 내려주는 의견 작성 시간 파싱 추가

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/datas/TopicReply.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/datas/TopicReply.java
@@ -3,12 +3,17 @@ package kr.co.tjoeun.apipractice_20200527.datas;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class TopicReply {
 
     private int id;
     private String content;
     private String side;
     private User writer;
+    private Calendar createdAt = Calendar.getInstance(); // 작성 일시 기록
 
     public static TopicReply getTopicReplyFromJson(JSONObject jsonObject) {
         TopicReply tr = new TopicReply();
@@ -26,7 +31,17 @@ public class TopicReply {
 //            활용해서 만든 User객체를 => 댓글의 작성자로 연결.
             tr.writer = User.getUserFromJson(user);
 
+//            String으로 들어오는 created_at을 => Calendar타입인 createdAt으로 변환.
+            String createdAtStr = jsonObject.getString("created_at");
+
+//            만들어져있는 createdAt 캘린더에 => 년/월/일/시 등 데이터 세팅. => setTime
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            tr.createdAt.setTime(sdf.parse(createdAtStr));
+
+
         } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
