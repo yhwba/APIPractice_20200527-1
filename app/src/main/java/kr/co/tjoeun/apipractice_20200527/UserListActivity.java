23057b5cce97bef24fbd13f2648464ef0a55273c
kj_cho <kj_cho@idealidea.co.kr>
2020-06-01 14:36:28 +0900

    사용자 목록 리스트뷰 / XML 작성

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/UserListActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/UserListActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserListActivity extends AppCompatActivity {
+public class UserListActivity extends BaseActivity {
 
 //    /user - GET 으로 접근해서, 사용자목록을 리스트뷰로 출력
 //    닉네임 (이메일주소) => 이 양식으로 표현.
@@ -13,5 +13,17 @@ public class UserListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
